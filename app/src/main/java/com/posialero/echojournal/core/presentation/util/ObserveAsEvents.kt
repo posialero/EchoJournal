224254a1c567ca9910e51fc1f1f988f504143b94
Daniel Najdrowski <danielnajdrowski@gmail.com>
2025-10-17 00:00:08 +0200

    Add ObserveAsEvents to core.presentation.util

---
+++ app/src/main/java/com/posialero/echojournal/core/presentation/util/ObserveAsEvents.kt
@@ -0,0 +1,31 @@
+package com.posialero.echojournal.core.presentation.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+@Composable
+fun <T> ObserveAsEvents(
+    flow: Flow<T>,
+    key1: Any? = null,
+    key2: Any? = null,
+    onEvent: (T) -> Unit
+) {
+    val lifecycle = LocalLifecycleOwner.current
+    LaunchedEffect(
+        lifecycle.lifecycle,
+        key1,
+        key2
+    ) {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.collect(onEvent)
+            }
+        }
+    }
+}
