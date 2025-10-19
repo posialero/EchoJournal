538f94634f9fe93e99b321abed2fc0e313c04999
Daniel Najdrowski <danielnajdrowski@gmail.com>
2025-10-19 23:39:33 +0200

    Added voice recording feature

--- app/src/main/java/com/posialero/echojournal/app/MainActivity.kt
+++ app/src/main/java/com/posialero/echojournal/app/MainActivity.kt
@@ -1,21 +1,14 @@
-package com.posialero.echojournal
+package com.posialero.echojournal.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.posialero.echojournal.core.presentation.designsystem.theme.EchoJournalTheme
-import com.posialero.echojournal.core.presentation.designsystem.theme.bgGradient
 import com.posialero.echojournal.echos.presentation.echos.EchosRoot
 
 class MainActivity : ComponentActivity() {
